23d8a622e63b3345fa569d21b76d66b2d3f6ab42
dungnguyen1709 <lethanhnghi135@yahoo.com>
2020-12-28 17:32:44 +0700

    commit

--- ex5_3.py
+++ ex5_3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-data = '''Dictionaries are sometimes found in other languages as “associative memories” or “associative arrays”. Unlike sequences, which are indexed by a range of numbers, dictionaries are indexed by keys, which can be any immutable type; strings and numbers can always be keys. Tuples can be used as keys if they contain only strings, numbers, or tuples; if a tuple contains any mutable object either directly or indirectly, it cannot be used as a key. You can’t use lists as keys, since lists can be modified in place using index assignments, slice assignments.''' # NOQA
+data = '''Dictionaries are sometimes found in other languages as “associative memories” or “associative arrays”. Unlike sequences, which are indexed by a range of numbers, dictionaries are indexed by keys, which can be any immutable type; strings and numbers can always be keys. Tuples can be used as keys if they contain only strings, numbers, or tuples; if a tuple contains any mutable object either directly or indirectly, it cannot be used as a key. You can’t use lists as keys, since lists can be modified in place using index assignments, slice assignments.'''  # NOQA
 
 
 # Chú ý: dấu “ không phải double quotes "
@@ -14,8 +14,13 @@ def solve(input_data):
     '''
     result = None
     # Xoá dòng raise và Viết code vào đây set result làm kết quả
-    raise NotImplementedError("Học viên chưa làm bài này")
-
+    import re
+    from collections import Counter
+    cleaned = re.sub(r'“|”|\.|,', ' ', data)
+    c = Counter(cleaned.split())
+    top = c.most_common(10)
+    result = top
+    print(result)
     return result
 
 
